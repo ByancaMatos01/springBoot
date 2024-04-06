c8f59024a2fb93fd0e3cd08a12ea859d63926193
Byanca <122841376+ByancaMatos01@users.noreply.github.com>
2024-04-06 10:11:43 -0300

    Add files via upload

---
+++ api (1)/api/src/test/java/evento/fatec/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package evento.fatec.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
